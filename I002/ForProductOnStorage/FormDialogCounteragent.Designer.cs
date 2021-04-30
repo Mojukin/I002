be8232a7216619f6a0d4f639a04a0952fdd20234
Salov Pavel <Pashtet080402@mail.ru>
2021-04-30 14:50:16 +0700

    Добавьте файлы проекта.

---
+++ I002/ForProductOnStorage/FormDialogCounteragent.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace I002
+{
+    partial class FormDialogCounteragent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.BtnChoose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LabFind = new System.Windows.Forms.Label();
+            this.TxtFindCounteragent = new System.Windows.Forms.TextBox();
+            this.tableForCounteragents = new System.Windows.Forms.DataGridView();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableForCounteragents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.BtnChoose);
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Controls.Add(this.LabFind);
+            this.MainPanel.Controls.Add(this.TxtFindCounteragent);
+            this.MainPanel.Controls.Add(this.tableForCounteragents);
+            this.MainPanel.Controls.Add(this.BtnClose);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1076, 477);
+            this.MainPanel.TabIndex = 4;
+            // 
+            // BtnChoose
+            // 
+            this.BtnChoose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnChoose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.BtnChoose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnChoose.ForeColor = System.Drawing.Color.Black;
+            this.BtnChoose.Location = new System.Drawing.Point(426, 398);
+            this.BtnChoose.Name = "BtnChoose";
+            this.BtnChoose.Size = new System.Drawing.Size(226, 39);
+            this.BtnChoose.TabIndex = 29;
+            this.BtnChoose.Text = "Выбрать контрагента";
+            this.BtnChoose.UseVisualStyleBackColor = true;
+            this.BtnChoose.Click += new System.EventHandler(this.BtnChoose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(520, 25);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Выберите контрагента, у которого хотите закупить товар";
+            // 
+            // LabFind
+            // 
+            this.LabFind.AutoSize = true;
+            this.LabFind.Location = new System.Drawing.Point(38, 93);
+            this.LabFind.Name = "LabFind";
+            this.LabFind.Size = new System.Drawing.Size(237, 25);
+            this.LabFind.TabIndex = 19;
+            this.LabFind.Text = "Ввод данных для поиска:";
+            // 
+            // TxtFindCounteragent
+            // 
+            this.TxtFindCounteragent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtFindCounteragent.Location = new System.Drawing.Point(43, 121);
+            this.TxtFindCounteragent.Name = "TxtFindCounteragent";
+            this.TxtFindCounteragent.Size = new System.Drawing.Size(818, 33);
+            this.TxtFindCounteragent.TabIndex = 18;
+            this.TxtFindCounteragent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TxtFindCounteragent.TextChanged += new System.EventHandler(this.TxtFindCounteragent_TextChanged);
+            // 
+            // tableForCounteragents
+            // 
+            this.tableForCounteragents.AllowUserToAddRows = false;
+            this.tableForCounteragents.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black;
+            this.tableForCounteragents.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.tableForCounteragents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tableForCounteragents.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.tableForCounteragents.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tableForCounteragents.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.tableForCounteragents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableForCounteragents.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tableForCounteragents.Location = new System.Drawing.Point(43, 160);
+            this.tableForCounteragents.MultiSelect = false;
+            this.tableForCounteragents.Name = "tableForCounteragents";
+            this.tableForCounteragents.ReadOnly = true;
+            this.tableForCounteragents.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.GrayText;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tableForCounteragents.RowHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.tableForCounteragents.RowHeadersWidth = 51;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.LightBlue;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.Black;
+            this.tableForCounteragents.RowsDefaultCellStyle = dataGridViewCellStyle8;
+            this.tableForCounteragents.RowTemplate.Height = 35;
+            this.tableForCounteragents.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tableForCounteragents.Size = new System.Drawing.Size(988, 206);
+            this.tableForCounteragents.TabIndex = 17;
+            this.tableForCounteragents.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tableForCounteragents_CellClick);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.BackgroundImage = global::I002.Properties.Resources.back;
+            this.BtnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClose.FlatAppearance.BorderSize = 0;
+            this.BtnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClose.ForeColor = System.Drawing.Color.Black;
+            this.BtnClose.Location = new System.Drawing.Point(12, 12);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(57, 49);
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // FormDialogCounteragent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 477);
+            this.ControlBox = false;
+            this.Controls.Add(this.MainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormDialogCounteragent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Выберите контрагента";
+            this.Load += new System.EventHandler(this.FormDialogCounteragent_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableForCounteragents)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button BtnChoose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LabFind;
+        private System.Windows.Forms.TextBox TxtFindCounteragent;
+        private System.Windows.Forms.DataGridView tableForCounteragents;
+        private System.Windows.Forms.Button BtnClose;
+    }
+}
---
+++ I002/ForProducts/FormAddProduct.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace I002
+{
+    partial class FormAddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.TxtAddProduct = new System.Windows.Forms.TextBox();
+            this.LabNameProduct = new System.Windows.Forms.Label();
+            this.BtnAddProduct = new System.Windows.Forms.Button();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.TxtAddProduct);
+            this.MainPanel.Controls.Add(this.LabNameProduct);
+            this.MainPanel.Controls.Add(this.BtnAddProduct);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(479, 251);
+            this.MainPanel.TabIndex = 1;
+            // 
+            // TxtAddProduct
+            // 
+            this.TxtAddProduct.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtAddProduct.Location = new System.Drawing.Point(76, 101);
+            this.TxtAddProduct.Name = "TxtAddProduct";
+            this.TxtAddProduct.Size = new System.Drawing.Size(319, 33);
+            this.TxtAddProduct.TabIndex = 8;
+            this.TxtAddProduct.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LabNameProduct
+            // 
+            this.LabNameProduct.AutoSize = true;
+            this.LabNameProduct.Location = new System.Drawing.Point(120, 49);
+            this.LabNameProduct.Name = "LabNameProduct";
+            this.LabNameProduct.Size = new System.Drawing.Size(243, 25);
+            this.LabNameProduct.TabIndex = 7;
+            this.LabNameProduct.Text = "Введите название товара:";
+            // 
+            // BtnAddProduct
+            // 
+            this.BtnAddProduct.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAddProduct.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.BtnAddProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAddProduct.ForeColor = System.Drawing.Color.Black;
+            this.BtnAddProduct.Location = new System.Drawing.Point(157, 174);
+            this.BtnAddProduct.Name = "BtnAddProduct";
+            this.BtnAddProduct.Size = new System.Drawing.Size(157, 39);
+            this.BtnAddProduct.TabIndex = 0;
+            this.BtnAddProduct.Text = "Добавить";
+            this.BtnAddProduct.UseVisualStyleBackColor = true;
+            this.BtnAddProduct.Click += new System.EventHandler(this.BtnAddProduct_Click);
+            // 
+            // FormAddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 251);
+            this.Controls.Add(this.MainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormAddProduct";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавление товара";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button BtnAddProduct;
+        private System.Windows.Forms.Label LabNameProduct;
+        private System.Windows.Forms.TextBox TxtAddProduct;
+    }
+}
